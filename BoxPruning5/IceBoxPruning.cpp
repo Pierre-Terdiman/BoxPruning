f7d030c9b529cfd924e26d1fd0f8a03e9110a536
Pierre-Terdiman <pierre.terdiman@gmail.com>
2017-02-15 10:16:22 +0100

    Part 5 in the "box pruning revisited" project
    See Reports\Part5.pdf for details

---
+++ BoxPruning5/IceBoxPruning.cpp
@@ -0,0 +1,200 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ *	Contains code for the "box pruning revisited" project.
+ *	\file		IceBoxPruning.cpp
+ *	\author		Pierre Terdiman
+ *	\date		February 2017
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Precompiled Header
+#include "Stdafx.h"
+
+using namespace Meshmerizer;
+
+// InsertionSort has better coherence, RadixSort is better for one-shot queries.
+#define PRUNING_SORTER	RadixSort
+//#define PRUNING_SORTER	InsertionSort
+
+static __forceinline int intersects2D(const AABB& a, const AABB& b)
+{
+       if(    b.mMax.y < a.mMin.y || a.mMax.y < b.mMin.y
+       ||     b.mMax.z < a.mMin.z || a.mMax.z < b.mMin.z)
+              return 0;
+/*       if(    b.mMax.y <= a.mMin.y || a.mMax.y < b.mMin.y
+       ||     b.mMax.z <= a.mMin.z || a.mMax.z < b.mMin.z)
+              return 0;*/
+       return 1;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ *	Bipartite box pruning. Returns a list of overlapping pairs of boxes, each box of the pair belongs to a different set.
+ *	\param		nb0		[in] number of boxes in the first set
+ *	\param		list0	[in] list of boxes for the first set
+ *	\param		nb1		[in] number of boxes in the second set
+ *	\param		list1	[in] list of boxes for the second set
+ *	\param		pairs	[out] list of overlapping pairs
+ *	\param		axes	[in] projection order (0,2,1 is often best)
+ *	\return		true if success.
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Meshmerizer::BipartiteBoxPruning(udword nb0, const AABB** list0, udword nb1, const AABB** list1, Container& pairs)
+{
+	// Checkings
+	if(!nb0 || !list0 || !nb1 || !list1)	return false;
+
+	// Allocate some temporary data
+	float* MinPosList0 = new float[nb0+1];
+	float* MinPosList1 = new float[nb1+1];
+
+	// 1) Build main lists using the primary axis
+	for(udword i=0;i<nb0;i++)
+		MinPosList0[i] = list0[i]->mMin.x;
+	MinPosList0[nb0] = FLT_MAX;
+	for(udword i=0;i<nb1;i++)
+		MinPosList1[i] = list1[i]->mMin.x;
+	MinPosList1[nb1] = FLT_MAX;
+
+	// 2) Sort the lists
+	static PRUNING_SORTER RS0, RS1;	// Static for coherence.
+	udword* Sorted0 = RS0.Sort(MinPosList0, nb0+1).GetRanks();
+	udword* Sorted1 = RS1.Sort(MinPosList1, nb1+1).GetRanks();
+
+	// 3) Prune the lists
+	udword Index0, Index1;
+
+	const udword* const LastSorted0 = &Sorted0[nb0];
+	const udword* const LastSorted1 = &Sorted1[nb1];
+	const udword* RunningAddress0 = Sorted0;
+	const udword* RunningAddress1 = Sorted1;
+
+	while(RunningAddress1<LastSorted1 && Sorted0<LastSorted0)
+	{
+		Index0 = *Sorted0++;
+
+		const float MinLimit = MinPosList0[Index0];
+		while(MinPosList1[*RunningAddress1]<MinLimit)
+			RunningAddress1++;
+
+		const udword* RunningAddress2_1 = RunningAddress1;
+
+		const float MaxLimit = list0[Index0]->mMax.x;
+		while(MinPosList1[Index1 = *RunningAddress2_1++]<=MaxLimit)
+		{
+			if(intersects2D(*list0[Index0], *list1[Index1]))
+			{
+				pairs.Add(Index0).Add(Index1);
+			}
+		}
+	}
+
+	////
+
+	while(RunningAddress0<LastSorted0 && Sorted1<LastSorted1)
+	{
+		Index0 = *Sorted1++;
+
+		const float MinLimit = MinPosList1[Index0];
+		while(MinPosList0[*RunningAddress0]<=MinLimit)
+			RunningAddress0++;
+
+		const udword* RunningAddress2_0 = RunningAddress0;
+
+		const float MaxLimit = list1[Index0]->mMax.x;
+		while(MinPosList0[Index1 = *RunningAddress2_0++]<=MaxLimit)
+		{
+			if(intersects2D(*list0[Index1], *list1[Index0]))
+			{
+				pairs.Add(Index1).Add(Index0);
+			}
+		}
+	}
+
+	DELETEARRAY(MinPosList1);
+	DELETEARRAY(MinPosList0);
+
+	return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ *	Complete box pruning. Returns a list of overlapping pairs of boxes, each box of the pair belongs to the same set.
+ *	\param		nb		[in] number of boxes
+ *	\param		list	[in] list of boxes
+ *	\param		pairs	[out] list of overlapping pairs
+ *	\param		axes	[in] projection order (0,2,1 is often best)
+ *	\return		true if success.
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Meshmerizer::CompleteBoxPruning(udword nb, const AABB** list, Container& pairs)
+{
+	// Checkings
+	if(!nb || !list)
+		return false;
+
+	// Catch axes
+//	const udword Axis0 = axes.Axis0;	// REMOVED
+//	const udword Axis1 = axes.Axis1;	// REMOVED
+//	const udword Axis2 = axes.Axis2;	// REMOVED
+
+	// Allocate some temporary data
+	float* PosList = new float[nb+1];
+
+	// 1) Build main list using the primary axis
+	for(udword i=0;i<nb;i++)
+//		PosList[i] = list[i]->GetMin(Axis0);	// MODIFIED
+		PosList[i] = list[i]->mMin.x;			// MODIFIED
+	PosList[nb] = FLT_MAX;
+
+	// 2) Sort the list
+	static PRUNING_SORTER RS;	// Static for coherence
+	const udword* Sorted = RS.Sort(PosList, nb+1).GetRanks();
+
+//CPUID
+	// 3) Prune the list
+	const udword* const LastSorted = &Sorted[nb];
+	const udword* RunningAddress = Sorted;
+	udword Index0, Index1;
+	while(RunningAddress<LastSorted && Sorted<LastSorted)
+	{
+		Index0 = *Sorted++;
+
+		const float MinLimit = PosList[Index0];
+//		while(RunningAddress<LastSorted && PosList[*RunningAddress++]<MinLimit);
+		while(PosList[*RunningAddress++]<MinLimit);
+
+		const udword* RunningAddress2 = RunningAddress;
+
+//		const float MaxLimit = list[Index0]->GetMax(Axis0);	// MODIFIED
+//CPUID
+		const float MaxLimit = list[Index0]->mMax.x;		// MODIFIED
+//CPUID
+//		while(RunningAddress2<LastSorted && PosList[Index1 = *RunningAddress2++]<=MaxLimit)
+		while(PosList[Index1 = *RunningAddress2++]<=MaxLimit)
+		{
+			if(Index0!=Index1)
+			{
+/*				if(list[Index0]->Intersect(*list[Index1], Axis1))
+				{
+					if(list[Index0]->Intersect(*list[Index1], Axis2))
+					{
+						pairs.Add(Index0).Add(Index1);
+					}
+				}*/
+
+				// MODIFIED
+				if(intersects2D(*list[Index0], *list[Index1]))
+				{
+					pairs.Add(Index0).Add(Index1);
+				}
+			}
+		}
+	}
+//CPUID
+
+	DELETEARRAY(PosList);
+
+	return true;
+}
---
+++ BoxPruning5/StdAfx.h
@@ -0,0 +1,26 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__7739E2A3_1E44_11D6_8B0F_0050BAC83302__INCLUDED_)
+#define AFX_STDAFX_H__7739E2A3_1E44_11D6_8B0F_0050BAC83302__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "..\Shared\StdAfx.h"
+
+#define USE_HARDCODED_AXES
+
+namespace Meshmerizer
+{
+	#include "IceBoxPruning.h"
+}
+using namespace Meshmerizer;
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__7739E2A3_1E44_11D6_8B0F_0050BAC83302__INCLUDED_)
